c452cad4bc7910d18678be46a57d04085309b42e
Etienne <etienne@opengis.dev>
2022-04-19 20:25:16 -0500

    loading-shape-files-in-postgis-with-qgis

--- app/Models/Monument.php
+++ app/Models/Monument.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Monument extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('withWkt', function (Builder $builder) {
+            $builder->addSelect(['id', 'name', 'image', 'created_at', 'updated_at']);
+            $builder->addSelect(DB::raw('ST_AsText(geom) as wkt'));
+        });
+    }
 }
